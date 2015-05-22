a8a9849af572a98f8183434918ac4482f2819b53
helmijab <jaballah.helmi@gmail.com>
2015-05-22 17:23:59 +0100

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5 by JOOB and will</div>
+				<div class="title">Laravel 5 by helmi and will</div>
 				<div class="quote">{{ Inspiring::quote() }}</div>
 			</div>
 		</div>
