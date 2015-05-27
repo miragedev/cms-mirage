e98d793fa27f073d846b35e6915cf934db089375
William Langlois <wla@net-sense.eu>
2015-05-27 11:12:57 +0200

    modify user

--- app/Modele/User.php
+++ app/Modele/User.php
@@ -1,4 +1,4 @@
-<?php namespace App;
+<?php namespace Modele\App;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
@@ -22,7 +22,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'email','pseudo', 'password'];
+	protected $fillable = ['name', 'email','pseudo','role','password'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
